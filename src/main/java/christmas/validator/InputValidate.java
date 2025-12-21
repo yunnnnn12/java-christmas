bcf40ebf9200fbc518076fcd852a3bc7c178fd9f
chaeyun_ <sonchaeyun@sonchaeyun-ui-MacBookAir.local>
2025-12-21 22:21:02 +0900

    fix: improve exception handling in date exception test

--- src/main/java/christmas/validator/InputValidate.java
+++ src/main/java/christmas/validator/InputValidate.java
@@ -1,13 +1,14 @@
 package christmas.validator;
 
 public class InputValidate {
-    public static void validateRange(String input){
+    public static int validateRange(String input){
         if(!input.matches("^(?:[1-9]|[12][0-9]|3[01])$")){
                 throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
-
-            }
-
         }
+        return Integer.parseInt(input);
+
 
     }
+
 }
+
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,2 +1,29 @@
-package christmas.view;public class InputView {
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import christmas.validator.InputValidate;
+
+public class InputView {
+    public int readDate() {
+        int date;
+        while(true){
+            System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+            String input = Console.readLine();
+            try{
+                date = InputValidate.validateRange(input);
+                break;
+            } catch(IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+        return date;
+    }
+
+    public String readMenuAndCount(){
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        String input = Console.readLine();
+
+        return input;
+    }
+
 }
