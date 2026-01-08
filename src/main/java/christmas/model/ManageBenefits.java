cfa2273a68143f3498553e0f9b1f512f202d1be0
chaeyun_ <sonchaeyun@sonchaeyun-ui-MacBookAir.local>
2026-01-08 16:52:58 +0900

    refactor: manage discounts, gift and calculate benefitPrice, afterDiscountPrice

--- src/main/java/christmas/model/ManageBenefits.java
+++ src/main/java/christmas/model/ManageBenefits.java
@@ -8,63 +8,60 @@ public class ManageBenefits {
     private int specialDiscount;
     private int giftPrice;
 
-
     private int totalBenefitPrice;
     private int afterDiscountPrice;
 
     public int getGiftPrice() {
         return giftPrice;
     }
-
     public void setGiftPrice(int giftPrice) {
         this.giftPrice = giftPrice;
     }
 
+
     public int getWeekdayDiscount() {
         return weekdayDiscount;
-
     }
-
     public void setWeekdayDiscount(int weekdayDiscount) {
         this.weekdayDiscount = weekdayDiscount;
     }
 
+
     public int getAfterDiscountPrice() {
         return afterDiscountPrice;
     }
-
     public void setAfterDiscountPrice(int afterDiscountPrice) {
         this.afterDiscountPrice = afterDiscountPrice;
     }
 
+
     public int getChristmasDiscount() {
         return christmasDiscount;
     }
-
     public void setChristmasDiscount(int christmasDiscount) {
         this.christmasDiscount = christmasDiscount;
     }
 
+
     public int getSpecialDiscount() {
         return specialDiscount;
     }
-
     public void setSpecialDiscount(int specialDiscount) {
         this.specialDiscount = specialDiscount;
     }
 
+
     public int getWeekendDiscount() {
         return weekendDiscount;
     }
-
     public void setWeekendDiscount(int weekendDiscount) {
         this.weekendDiscount = weekendDiscount;
     }
 
+
     public int getTotalBenefitPrice() {
         return this.totalBenefitPrice;
     }
-
     public void setTotalBenefitPrice(ManageBenefits benefits) {
         this.totalBenefitPrice = benefits.getWeekdayDiscount() + benefits.getWeekendDiscount() + benefits.getSpecialDiscount() + benefits.getChristmasDiscount() ;
     }
