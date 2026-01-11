24fae33b5acf12050372e4bb946d704ae3d46f9f
chaeyun_ <sonchaeyun@sonchaeyun-ui-MacBookAir.local>
2026-01-11 17:37:51 +0900

    refactor: fix menu validation

--- src/main/java/christmas/validator/OrderInputValidator.java
+++ src/main/java/christmas/validator/OrderInputValidator.java
@@ -36,7 +36,6 @@ public class OrderInputValidator {
         if(!inputs.matches("(.*)-(.*)")){
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
-
     }
 
     public static void validateDuplicate(String inputs){
